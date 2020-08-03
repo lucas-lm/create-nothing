c1556f2a80554fa55a51b2adbf201c60fe55f23f
Lucas Miranda <lucas-m@outlook.com>
2020-08-02 23:15:20 -0300

    feat: function to parse args

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,29 @@
+import arg from 'arg'
+
+function parseArgumentsIntoOptions(rawArgs) {
+  const args = arg(
+    {
+      '--git': Boolean,
+      '--yes': Boolean,
+      '--install': Boolean,
+      '-g': '--git',
+      '-y': '--yes',
+      '-i': '--install'
+    },
+    {
+      argv: rawArgs.slice(2)
+    }
+  )
+
+  return {
+    template: args._[0],
+    skipPrompts: args['--yes'] || false,
+    git: args['--git'] || false,
+    runInstall: args['--install'] || false
+  }
+}
+
 export function cli(args) {
-  console.log(args)
+  const options = parseArgumentsIntoOptions(args)
+  console.log(options)
 }
